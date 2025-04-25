fdc931455d686ce04b34873e5d0d6a40eb6f6895
Константин Афанасьев <kosty-afanasev@ya.ru>
2025-04-25 22:20:56 +0400

    Recommendations

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -4,8 +4,34 @@ import "../../styles/Recommendations.scss"; // Импорт SCSS
 
 const Recommendations = ({ title }) => {
   return (
-    <div>
-      <h2>Рекомендации</h2>
+    <div className="recommendations">
+      <h2>Лучшие предложения</h2>
+      <div className="recommendations-container">
+        <div className="card">
+          <div className="image-holder">
+            <img src="../../../images/yandex-market.png"></img>
+          </div>
+          <div className="card-body">
+            <p className="cashback">Кэшбэк 4%</p>
+            <div className="card-body-description">
+              <p className="title">Яндекс. Маркет</p>
+              <p className="organization">Онлайн маркетплейс</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="image-holder">
+            <img src="../../../images/hoff.png"></img>
+          </div>
+          <div className="card-body">
+            <p className="cashback">Кэшбэк 100%</p>
+            <div className="card-body-description">
+              <p className="title">Hoff</p>
+              <p className="organization">Гипермаркеты мебели</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
