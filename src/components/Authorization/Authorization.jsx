a444a205ee993ceef08232085971127c601a252f
1nastya3425 <3425nastya@gmail.com>
2025-04-26 15:49:16 +0400

    log

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,13 +1,13 @@
-// src/components/Login/Login.jsx
+// src/components/Authorization/Authorization.jsx
 import React from 'react';
 import "../../styles/Login.scss";
 import { Link } from 'react-router-dom'; // Импортируем Link
 
-const Login = () => {
+const Authorization = () => {
   return (
     <div className="auth-bg">
       <div className="auth-main">
-        <h2 className="auth-title">Вход</h2>
+        <h2 className="auth-title">Регистрация</h2>
         <div className="auth-container">
           <div className="auth-input">
             <input
@@ -17,18 +17,30 @@ const Login = () => {
           </div>
           <div className="auth-input">
             <input
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="auth-input">
+            <input
               type="text"
               placeholder="Пароль"
             />
           </div>
+          <div className="auth-input">
+            <input
+              type="text"
+              placeholder="Повторите пароль"
+            />
+          </div>
         </div>
         <button className="auth-button">Войти</button>
-        <Link to="/authorization" className="auth-link">
-          Зарегистрироваться
+        <Link to="/login" className="auth-link">
+          Вернуться к входу
         </Link>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Authorization;
